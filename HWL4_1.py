780ac9777faf743bf299967db083b12b991bd9ac
MishaResh <65731478+MishaResh@users.noreply.github.com>
2021-10-26 19:13:22 +0400

    Файл работает в консоли

--- HWL4_1.py
+++ HWL4_1.py
@@ -4,5 +4,16 @@
 
 from sys import argv
 
-script_name, first_param, second_param, third_param = argv
+script_name, production, time_rate, plan_execution = argv
+c_salary = int(production)*int(time_rate)
+bonus = c_salary*int(plan_execution)/100
+if int(plan_execution) >= 100:
+    print(f'Выработка в часах {production} * '
+          f'ставка в час {time_rate} + '
+          f' премия {bonus} = {c_salary+bonus}')
+else:
+    print(f'Выработка в часах {production} * '
+          f'ставка в час {time_rate} + '
+          f' премия {0} = {c_salary}')
+
 
