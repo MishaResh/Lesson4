a14a6addc5b579fd949593648d0f7685fb204b78
MishaResh <65731478+MishaResh@users.noreply.github.com>
2021-10-27 18:24:12 +0400

    Правильно работающий файл

--- HWL4_5.py
+++ HWL4_5.py
@@ -4,14 +4,7 @@
 # Подсказка: использовать функцию reduce().
 
 from functools import reduce
-my_list = [i for i in range(100, 1000) if i % 2 == 0]
 
-
-def my_func(list):
-    res = 1
-    for x in list:
-        res = res * x
-    print(res)
-
-
-print(reduce(my_func, my_list))
+my_list = [i for i in range(100, 1002) if i % 2 == 0]
+print(my_list)
+print(reduce(lambda x, y: x * y, my_list))
